3c995ce07a0b9e63b795a2ccbb55fd58d8a618f3
Greg Bueno <greg@gregbueno.com>
2014-11-12 17:14:56 -0800

    Adding mu-plugins config file.

---
+++ movabletype-id-mapper-mu-plugins.php
@@ -0,0 +1,11 @@
+<?php
+/*
+  Plugin Name: Movable Type ID Mapper
+  Plugin URI: https://bitbucket.org/NemesisVex/movable-type-id-mapper-for-wordpress
+  Description: Movable Type ID Mapper looks up an entry from mt_entry by entry_id, then queries Wordpress for a corresponding entry by entry_basename.
+  Author: Greg Bueno
+  Version: 1.0.1
+  Author URI: http://vigilantmedia.com/
+ */
+
+require WPMU_PLUGIN_DIR.'/movabletype-id-mapper/movabletype-id-mapper.php';
